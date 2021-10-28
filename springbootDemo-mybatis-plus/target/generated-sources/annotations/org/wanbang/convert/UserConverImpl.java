4b4a1929a7fdc00acb934945d991595105a6c976
马佳健 <1061902084@qq.com>
2021-10-28 21:07:26 +0800

    commit

--- springbootDemo-mybatis-plus/target/generated-sources/annotations/org/wanbang/convert/UserConverImpl.java
+++ springbootDemo-mybatis-plus/target/generated-sources/annotations/org/wanbang/convert/UserConverImpl.java
@@ -8,7 +8,7 @@ import org.wanbang.entity.common.UserVo1;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-10-19T17:11:43+0800",
+    date = "2021-10-28T20:48:33+0800",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_221 (Oracle Corporation)"
 )
 @Component
