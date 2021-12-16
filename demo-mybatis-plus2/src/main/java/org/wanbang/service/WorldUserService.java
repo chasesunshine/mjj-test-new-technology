cddc76413c121ed0329dc5c4ea21a45a777542c2
马佳健 <13917186462@163.com>
2021-12-16 09:55:24 +0800

    个人测

--- demo-mybatis-plus2/src/main/java/org/wanbang/service/WorldUserService.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/service/WorldUserService.java
@@ -40,4 +40,15 @@ public class WorldUserService {
         return mjj;
     }
 
+
+    public Integer insert1(String name,Integer age) {
+        int mjj = userMapper.insert(WorldUser.builder().name(name).age(age).build());
+
+        if(true){
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
+
+        return mjj;
+    }
+
 }
