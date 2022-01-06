afabe28fb6a070f5658990698f0560ad798fcea1
马佳健 <13917186462@163.com>
2022-01-06 18:03:16 +0800

    本地线程ThreadLocal

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/ThreadLocal/ThreadLocalTest1/TestClient.java
@@ -0,0 +1,29 @@
+package org.wanbang.study.ThreadLocal.ThreadLocalTest1;
+
+public class TestClient implements Runnable {
+    private ThreadLocalUtil sn;
+
+    public TestClient(ThreadLocalUtil sn) {
+        super();
+        this.sn = sn;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            // ④每个线程打出3个序列值
+            System.out.println("thread[" + Thread.currentThread().getName() + "] sn[" + sn.getNextNum() + "]");
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadLocalUtil sn = new ThreadLocalUtil();
+        // ③ 3个线程共享sn，各自产生序列号
+        TestClient t1 = new TestClient(sn);
+        TestClient t2 = new TestClient(sn);
+        TestClient t3 = new TestClient(sn);
+        new Thread(t1).start();
+        new Thread(t2).start();
+        new Thread(t3).start();
+    }
+}
---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/ThreadLocal/ThreadLocalTest2/ThreadLocalTestDateUtilThreadLocal.java
@@ -0,0 +1,17 @@
+package org.wanbang.study.ThreadLocal.ThreadLocalTest2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// https://cloud.tencent.com/developer/article/1442006
+public class ThreadLocalTestDateUtilThreadLocal {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 20; i++) {
+            service.execute(()->{
+                System.out.println(DateUtilThreadLocal.parse("2019-06-01 16:34:30"));
+            });
+        }
+        service.shutdown();
+    }
+}
