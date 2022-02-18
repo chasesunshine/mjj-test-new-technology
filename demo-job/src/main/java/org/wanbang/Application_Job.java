6cca0dedc19ec7cfcb69396e47eb6625366f8471
马佳健 <majiajian@fosunhealth.com>
2022-02-18 16:37:46 +0800

    定时任务执行

---
+++ demo-job/src/main/java/org/wanbang/Application_Job.java
@@ -0,0 +1,18 @@
+package org.wanbang;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling //开启定时任务
+@SpringBootApplication
+@MapperScan("org.wanbang.mapper")	//程序会自动为mybatis创建代理对象
+public class Application_Job {
+	
+	//定义主启动类
+	public static void main(String[] args) {
+		//启动SpringBoot程序
+		SpringApplication.run(Application_Job.class, args);
+	}
+}
