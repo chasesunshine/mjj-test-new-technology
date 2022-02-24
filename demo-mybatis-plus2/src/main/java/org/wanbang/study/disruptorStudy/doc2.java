62107bf09b7137820d87e37316a8268db319e765
马佳健 <majiajian@fosunhealth.com>
2022-02-24 18:28:01 +0800

    disruptor 基本用例

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/disruptorStudy/doc2.java
@@ -0,0 +1,16 @@
+package org.wanbang.study.disruptorStudy;
+
+public class doc2 {
+    // Disruptor 系列（二）使用场景
+    // https://www.cnblogs.com/binarylei/p/9221560.html
+
+    // 今天用一个订单问题来加深对 Disruptor 的理解。当系统中有订单产生时，系统首先会记录订单信息。同时也会发送消息到其他系统处理相关业务，最后才是订单的处理。
+    //    代码包含以下内容：
+    //    1） 事件对象 Event
+    //    2）三个消费者 Handler
+    //    3）一个生产者 Producer
+    //    4）执行 Main 方法
+
+
+
+}
--- demo-mybatis-plus2/src/test/java/org/wanbang/test/TestMybatis.java
+++ demo-mybatis-plus2/src/test/java/org/wanbang/test/TestMybatis.java
@@ -12,6 +12,7 @@ import org.wanbang.mapper.CityMapper;
 import org.wanbang.entity.City;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -67,6 +68,22 @@ public class TestMybatis {
 		String value = "、1、2、3";
 		String substring = value.substring(1);
 		System.out.println(substring);
+	}
+
+	@Test
+	public void testQueryUser6() throws InterruptedException {
+		Date date = new Date();
+		long time = date.getTime();
+
+		for (int i = 0; i < 2; i++) {
+			int a = i;
+			Thread.sleep(1000);
+		}
+
+		Date date2 = new Date();
+		long time1 = date2.getTime();
+
+		System.out.println(time1 - time);
 	}
 
 }
