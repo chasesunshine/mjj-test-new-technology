d1e2e121fdc9c5156fd5b57329b1c97a8a3e374a
马佳健 <1061902084@qq.com>
2022-02-27 20:56:14 +0800

    循环依赖

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/springCircleDependence/doc.java
@@ -0,0 +1,4 @@
+package org.wanbang.study.springCircleDependence;
+
+public class doc {
+}
---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/springCircleDependence/test1/TestService1.java
@@ -0,0 +1,20 @@
+package org.wanbang.study.springCircleDependence.test1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService1 {
+    @Autowired
+    private TestService2 testService2;
+
+    // 使用构造方法注入 - 循环依赖
+    public TestService1(TestService2 testService2) {
+        this.testService2 = testService2;
+    }
+
+    public void test1() {
+        System.out.println("test1");
+    }
+}
