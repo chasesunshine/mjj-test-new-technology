dd17851c062ccb443f0fc425a49f73d21b1fb8f4
马佳健 <majiajian@dongfangfuli.com>
2022-06-06 14:48:58 +0800

    MD5测试

--- demo-mybatis-plus2/src/test/java/org/wanbang/test/TestMybatis.java
+++ demo-mybatis-plus2/src/test/java/org/wanbang/test/TestMybatis.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.sun.xml.internal.ws.util.UtilException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ import org.wanbang.entity.City;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -28,6 +33,19 @@ public class TestMybatis {
 
 	@Resource
 	private CityMapper cityMapper;
+
+	private static final Charset ENCODER = StandardCharsets.UTF_8;
+	private static final MessageDigest MESSAGE;
+	private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	private static final int NUM_16 = 16;
+
+	static {
+		try {
+			MESSAGE = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new UtilException("获取MD5 对象失败", e);
+		}
+	}
 
 	@Test
 	public void testQueryUser1() {
@@ -119,8 +137,33 @@ public class TestMybatis {
 
 	@Test
 	public void testQueryUser8() {
-		String value = "、1、2、3";
-		String substring = value.substring(1);
-		System.out.println(substring);
+		String str = "mjj12321321";
+		try {
+			MESSAGE.update(str.getBytes(ENCODER));
+			byte[] b = MESSAGE.digest();
+
+			StringBuilder digestHexStr = new StringBuilder();
+			for (int i = 0; i < NUM_16; i++) {
+				digestHexStr.append(byte2HexStr(b[i]));
+			}
+			String s = digestHexStr.toString();
+			System.out.println("数据 ："+ s);
+		} catch (Exception e) {
+			throw new UtilException("Md5加密失败", e);
+		}
 	}
+
+		/**
+		 * 转化成16 进制
+		 *
+		 * @param ib byte
+		 * @return java.lang.String
+		 */
+		private static String byte2HexStr(byte ib) {
+			char[] ob = new char[2];
+			ob[0] = HEX_DIGITS[(ib >>> 4 & 0xF)];
+			ob[1] = HEX_DIGITS[(ib & 0xF)];
+			return new String(ob);
+		}
+
 }
