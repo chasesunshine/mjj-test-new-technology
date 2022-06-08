92fe1c15dd9e236c61718f6c8caf8ce3c1526d66
马佳健 <majiajian@dongfangfuli.com>
2022-06-09 00:03:29 +0800

    新项目 - springstudy

---
+++ spring-study/src/main/java/com/dongfu/Application_Spring.java
@@ -0,0 +1,14 @@
+package com.dongfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application_Spring {
+	
+	//定义主启动类
+	public static void main(String[] args) {
+		//启动SpringBoot程序
+		SpringApplication.run(Application_Spring.class, args);
+	}
+}
