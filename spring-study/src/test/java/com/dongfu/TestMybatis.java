28e3d4aaca332ed1899ad183a3f8b6915d0f4a1c
马佳健 <majiajian@dongfangfuli.com>
2022-06-09 00:34:32 +0800

    mybatis配置

--- spring-study/src/test/java/com/dongfu/TestMybatis.java
+++ spring-study/src/test/java/com/dongfu/TestMybatis.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 
 @RunWith(SpringRunner.class)	//注意测试文件的位置 必须在主文件加载包路径下
 @SpringBootTest
@@ -12,6 +16,11 @@ public class TestMybatis {
 
 	@Test
 	public void test(){
-		System.out.println("123");
+		Date date = new Date();
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		calendar.add(calendar.DATE,1);
+		date = calendar.getTime();
+		System.out.println(date);
 	}
 }
