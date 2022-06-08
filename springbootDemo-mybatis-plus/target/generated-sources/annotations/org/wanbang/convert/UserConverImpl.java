8c70c87b8fa08fa93d4d67b3d2bbf01fb22ccc1f
马佳健 <majiajian@dongfangfuli.com>
2022-06-08 15:25:38 +0800

    日志打印

--- springbootDemo-mybatis-plus/target/generated-sources/annotations/org/wanbang/convert/UserConverImpl.java
+++ springbootDemo-mybatis-plus/target/generated-sources/annotations/org/wanbang/convert/UserConverImpl.java
@@ -8,7 +8,7 @@ import org.wanbang.entity.common.UserVo1;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-02T09:15:49+0800",
+    date = "2022-06-08T13:37:30+0800",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_221 (Oracle Corporation)"
 )
 @Component
