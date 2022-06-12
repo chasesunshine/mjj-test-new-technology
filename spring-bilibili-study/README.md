6c7628f0dbadf8b08c0b699ec9a48c6327438835
马佳健 <majiajian@dongfangfuli.com>
2022-06-13 00:18:39 +0800

    P5  【手写Spring】手写Spring之扫描逻辑（下）和BeanDefinition对象

--- spring-bilibili-study/README.md
+++ spring-bilibili-study/README.md
@@ -2,3 +2,4 @@
 # P2 【手写Spring】手写Spring前Spring用法复习
 # P3 【手写Spring】手写Spring准备工作
 # P4 【手写Spring】手写Spring之启动和扫描逻辑模拟实现（上）
+# P5 【手写Spring】手写Spring之扫描逻辑（下）和BeanDefinition对象
--- spring-bilibili-study/src/main/java/com/spring/ZhouyuApplicationContext.java
+++ spring-bilibili-study/src/main/java/com/spring/ZhouyuApplicationContext.java
@@ -1,6 +1,8 @@
 package com.spring;
 
+import java.io.File;
 import java.lang.annotation.Annotation;
+import java.net.URL;
 
 public class ZhouyuApplicationContext {
     private Class configClass;
@@ -10,7 +12,41 @@ public class ZhouyuApplicationContext {
 
         // 解析配置类
         // ComponentScan注解 --> 扫描路径 --> 扫描
-        Annotation[] annotations = configClass.getAnnotations();
+        ComponentScan componentScan = (ComponentScan) configClass.getDeclaredAnnotation(ComponentScan.class);
+        String path = componentScan.value(); //扫描路径
+        path = path.replace(".","/");
+
+        // 扫描 三种类加载器
+        // Bootstrap -->jre/lib
+        // Ert -->jre/ext/lib
+        // App -->classpath-->
+        ClassLoader classLoader = ZhouyuApplicationContext.class.getClassLoader(); //app
+        URL resource = classLoader.getResource(path);
+        File file = new File((resource.getFile()));
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            for (File f:files) {
+
+                String fileName = f.getAbsolutePath();
+                // 将 C:\AllJavaWorkSpace\IdeaWorkSpace\mjj-test-new-technology\spring-bilibili-study\target\classes\com\zhouyu\service\UserService.class 转换为 com.zhouyu.service.UserService
+                if(fileName.endsWith(".class")){
+                    String className = fileName.substring(fileName.indexOf("com"), fileName.indexOf(".class"));
+                    className = className.replace("\\",".");
+                    System.out.println(className);
+
+                    Class<?> aClass = null;
+                    try {
+                        aClass = classLoader.loadClass(className);
+                        if(aClass.isAnnotationPresent(Component.class)){
+                            // 当前类 是 一个bean
+                        }
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
 
     }
 
