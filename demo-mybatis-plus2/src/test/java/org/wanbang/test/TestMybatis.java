bbbe4beb46fe6e2eb304b3d070f7f6b6cc6d9557
马佳健 <majiajian@dongfangfuli.com>
2022-06-19 14:20:47 +0800

    commit

--- demo-mybatis-plus2/src/test/java/org/wanbang/test/TestMybatis.java
+++ demo-mybatis-plus2/src/test/java/org/wanbang/test/TestMybatis.java
@@ -22,10 +22,8 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)	//注意测试文件的位置 必须在主文件加载包路径下
 @SpringBootTest
@@ -153,17 +151,58 @@ public class TestMybatis {
 		}
 	}
 
-		/**
-		 * 转化成16 进制
-		 *
-		 * @param ib byte
-		 * @return java.lang.String
-		 */
-		private static String byte2HexStr(byte ib) {
-			char[] ob = new char[2];
-			ob[0] = HEX_DIGITS[(ib >>> 4 & 0xF)];
-			ob[1] = HEX_DIGITS[(ib & 0xF)];
-			return new String(ob);
-		}
+	/**
+	 * 转化成16 进制
+	 *
+	 * @param ib byte
+	 * @return java.lang.String
+	 */
+	private static String byte2HexStr(byte ib) {
+		char[] ob = new char[2];
+		ob[0] = HEX_DIGITS[(ib >>> 4 & 0xF)];
+		ob[1] = HEX_DIGITS[(ib & 0xF)];
+		return new String(ob);
+	}
+
+	/**
+	 * 转化成16 进制
+	 *
+	 * @param ib byte
+	 * @return java.lang.String
+	 */
+	@Test
+	public void byte2HexStr2() {
+		Map<String, Integer> unsortMap = new HashMap<>();
+		unsortMap.put("z", 10);
+		unsortMap.put("b", 5);
+		unsortMap.put("a", 6);
+		unsortMap.put("c", 20);
+		unsortMap.put("d", 1);
+		unsortMap.put("e", 7);
+		unsortMap.put("y", 8);
+		unsortMap.put("n", 99);
+		unsortMap.put("g", 50);
+		unsortMap.put("m", 2);
+		unsortMap.put("f", 9);
+
+		System.out.println("Original...");
+		System.out.println(unsortMap);
+
+		// sort by values, and reserve it, 10,9,8,7,6...
+		Map<String, Integer> result = unsortMap.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+						(oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		// Alternative way
+		Map<String, Integer> result2 = new LinkedHashMap<>();
+		unsortMap.entrySet().stream()
+				.sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+				.forEachOrdered(x -> result2.put(x.getKey(), x.getValue()));
+
+		System.out.println("Sorted...");
+		System.out.println(result);
+		System.out.println(result2);
+	}
 
 }
