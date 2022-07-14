3dad84e342fee21750f51579800f162eb4db877b
马佳健 <majiajian@dongfangfuli.com>
2022-07-14 15:02:53 +0800

    redis分布式锁 - 简单配置

---
+++ demo-mybatis-plus3/src/main/java/org/wanbang/controller/RedisController.java
@@ -0,0 +1,44 @@
+package org.wanbang.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.wanbang.common.entity.Result;
+import org.wanbang.entity.SpringWorld;
+import org.wanbang.service.SpringWordService;
+import org.wanbang.service.TestService;
+import org.wanbang.util.redis.lock.LockExecutor;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * @description: TODO
+ * @author majiajian
+ * @date 2022/6/22 16:14
+ * @version 1.0
+ */
+
+@Component
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+    @Resource
+    private SpringWordService springWordService;
+
+    @Resource
+    private LockExecutor lockExecutor;
+
+    @GetMapping("/selectOne")
+    public void  selectOne(){
+        try {
+            String mjj1 = lockExecutor.exec("Mjj:token", 3, () -> {
+                return "1";
+            });
+            System.out.println("1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ demo-mybatis-plus3/src/main/java/org/wanbang/util/redis/lock/LockExecutor.java
@@ -0,0 +1,43 @@
+package org.wanbang.util.redis.lock;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+/**
+ * @author chasesunshine
+ */
+@Component
+@AllArgsConstructor
+@Slf4j
+public class LockExecutor {
+
+    private final RedissonClient redissonClient;
+
+    public <T>T exec(String lockKey, long seconds, Supplier<T> supplier) throws Exception {
+
+        RLock lock = redissonClient.getLock(lockKey);
+        if (lock.isLocked()) {
+            throw new RuntimeException("获取锁失败");
+        }
+
+        try {
+            if (lock.tryLock(seconds, TimeUnit.SECONDS)) {
+                return supplier.get();
+            } else {
+                throw new RuntimeException("获取锁失败");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("获取锁失败");
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+}
