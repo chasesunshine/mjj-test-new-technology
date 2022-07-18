47145d14fbeb626e112f0b0fa344c1d0cc269a4a
马佳健 <majiajian@dongfangfuli.com>
2022-07-18 15:02:34 +0800

    适配器模式

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/designMode/adapter/NewActionB.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/designMode/adapter/NewActionB.java
@@ -3,6 +3,6 @@ package org.wanbang.study.designMode.adapter;
 // 新的类实现
 public class NewActionB implements NewAction{
     public void run(){
-        System.out.println("run开始工作");
+        System.out.println("NewActionB run开始工作");
     }
 }
