fd6baddf5df6da797c91fdfe93455a2b0118a874
马佳健 <majiajian@dongfangfuli.com>
2022-07-30 13:07:48 +0800

    commit

--- demo-mybatis-plus3/src/main/java/org/wanbang/controller/SpringWordController.java
+++ demo-mybatis-plus3/src/main/java/org/wanbang/controller/SpringWordController.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * (SpringWord)表控制层
@@ -23,42 +24,51 @@ public class SpringWordController {
     private SpringWordService springWordService;
 
     @GetMapping("/selectOne")
-    public String  selectOne(){
+    public String selectOne() {
         SpringWorld springWorld = springWordService.queryById((long) 1);
         return JSON.toJSONString(springWorld);
     }
 
-//    public static void main(String[] args) {
-//        SpringWorld build = SpringWorld.builder().age(1).value(3).build();
-//        SpringWorld build1 = SpringWorld.builder().age(2).value(3).build();
-//        SpringWorld build2 = SpringWorld.builder().age(1).value(4).build();
-//        SpringWorld build3 = SpringWorld.builder().age(2).value(4).build();
-//        SpringWorld build4 = SpringWorld.builder().age(2).value(4).build();
-//        SpringWorld build5 = SpringWorld.builder().age(3).value(5).build();
-//        SpringWorld build6 = SpringWorld.builder().age(1).value(5).build();
-//
-//        List<SpringWorld> springWorlds = Arrays.asList(build, build1, build2, build3, build4, build5, build6);
-//
-//        System.out.println(JSON.toJSONString(springWorlds));
-//
-//        Arrays.sort(springWorlds, new Comparator<SpringWorld>() {
-//            @Override
-//            public int compare(SpringWorld o1, SpringWorld o2) {
-//                Integer age1 = o1.getAge();
-//                Integer age2 = o2.getAge();
-//                Integer value1 = o1.getValue();
-//                Integer value2 = o2.getValue();
-//                if(age1 > age2){
-//                    return 1;
-//                }
-//                if(age1 == age2){
-//                    if(value1 > value2){
-//                        return 1;
-//                    }
-//                }
-//            }
-//        });
-//    }
+    public static void main(String[] args) {
+        SpringWorld build = SpringWorld.builder().age(1).value(3).build();
+        SpringWorld build1 = SpringWorld.builder().age(2).value(3).build();
+        SpringWorld build2 = SpringWorld.builder().age(1).value(4).build();
+        SpringWorld build3 = SpringWorld.builder().age(2).value(4).build();
+        SpringWorld build4 = SpringWorld.builder().age(2).value(4).build();
+        SpringWorld build5 = SpringWorld.builder().age(3).value(5).build();
+        SpringWorld build6 = SpringWorld.builder().age(1).value(5).build();
 
+        List<SpringWorld> springWorlds = Arrays.asList(build, build1, build2, build3, build4, build5, build6);
+
+        System.out.println(JSON.toJSONString(springWorlds));
+
+        Comparator<Object> comparator = (o1, o2) -> {
+            Integer age1 = ((SpringWorld) o1).getAge();
+            Integer age2 = ((SpringWorld) o2).getAge();
+
+            Integer value1 = ((SpringWorld) o1).getValue();
+            Integer value2 = ((SpringWorld) o2).getValue();
+
+            if (age1.compareTo(age2) != 0) {
+                return age1.compareTo(age2);
+            }else {
+                if(value1.compareTo(value2) != 0){
+                    if(value1 > value2){
+                        return -1;
+                    }else {
+                        return 1;
+                    }
+                }else {
+                    return 0;
+                }
+            }
+        };
+
+        List<SpringWorld> collect = springWorlds.stream().sorted(comparator.reversed()).collect(Collectors.toList());
+        List<SpringWorld> collect1 = springWorlds.stream().sorted(comparator).collect(Collectors.toList());
+
+        System.out.println(JSON.toJSONString(collect));
+        System.out.println(JSON.toJSONString(collect1));
+    }
 }
 
