f5f0b398f98f8a69db837ee3ec5041e6c7636f3e
马佳健 <majiajian@dongfangfuli.com>
2022-08-11 18:06:45 +0800

    桥接模式

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/bridgeMode/doc.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/bridgeMode/doc.java
@@ -8,7 +8,7 @@ package org.wanbang.study.allDesignMode.constructMode.bridgeMode;
 
 /**
  * 桥接模式 : 桥接模式的主要作⽤就是通过将抽象部分与实现部分分离，把多种可匹配的使⽤进⾏组合。说⽩了核⼼
- *          实现也就是在A类中含有B类接⼝，通过构造函数传递B类的实现，这个B类就是设计的 桥 。
+ *          实现也就是在A类中含有B类接⼝，通过构造函数传递B类的实现，这个B类就是设计的桥 。
  */
 public class doc {
 
