04defaa5dca28f3f04c8bc6a1061e9afa3ced8e8
马佳健 <majiajian@dongfangfuli.com>
2022-08-11 13:15:56 +0800

    代理模式详解

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/proxyMode/Client.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/proxyMode/Client.java
@@ -18,8 +18,7 @@ public class Client {
     public static void main(String[] args) {
         //    我们要代理的真实对象
         Subject realSubject = new RealSubject();
-
-        //    我们要代理哪个真实对象，就将该对象传进去，最后是通过该真实对象来调用其方法的
+        //    我们要代理哪个真实对象，就将该对象传进去，最后是通过  该真实对象  来调用其方法的
         InvocationHandler handler = new DynamicProxy(realSubject);
 
         /*
