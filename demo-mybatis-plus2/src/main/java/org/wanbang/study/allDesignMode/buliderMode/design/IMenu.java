08d4297d8304d57edc32f8f2294bedcc07ec89fc
马佳健 <majiajian@dongfangfuli.com>
2022-08-11 14:02:02 +0800

    建造者模式

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/buliderMode/design/IMenu.java
@@ -0,0 +1,22 @@
+package org.wanbang.study.allDesignMode.buliderMode.design;
+
+import org.wanbang.study.allDesignMode.buliderMode.service.Matter;
+
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/11 13:34
+* @version 1.0
+*/
+
+public interface IMenu {
+    IMenu appendCeiling(Matter matter); // 吊顶
+
+    IMenu appendCoat(Matter matter); // 涂料
+
+    IMenu appendFloor(Matter matter); // 地板
+
+    IMenu appendTile(Matter matter); // 地砖
+
+    String getDetail(); // 明细
+}
---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/buliderMode/service/impl/LevelOneCeiling.java
@@ -0,0 +1,30 @@
+package org.wanbang.study.allDesignMode.buliderMode.service.impl;
+
+import org.wanbang.study.allDesignMode.buliderMode.service.Matter;
+
+import java.math.BigDecimal;
+
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/11 13:37
+* @version 1.0
+*/
+
+public class LevelOneCeiling implements Matter {
+    public String scene() {
+        return "吊顶";
+    }
+    public String brand() {
+        return "装修公司⾃带";
+    }
+    public String model() {
+        return "⼀级顶";
+    }
+    public BigDecimal price() {
+        return new BigDecimal(260);
+    }
+    public String desc() {
+        return "造型只做低⼀级，只有⼀个层次的吊顶，⼀般离顶120-150mm";
+    }
+}
