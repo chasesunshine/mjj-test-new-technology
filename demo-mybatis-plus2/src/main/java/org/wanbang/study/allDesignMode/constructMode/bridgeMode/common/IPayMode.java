aa497c9d14a467420bfe34c5a01af222f2d35758
马佳健 <majiajian@dongfangfuli.com>
2022-08-11 18:01:08 +0800

    桥接模式

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/bridgeMode/common/IPayMode.java
@@ -0,0 +1,15 @@
+package org.wanbang.study.allDesignMode.constructMode.bridgeMode.common;
+/**
+* @description: 定义⽀付模式接⼝
+* @author majiajian
+* @date 2022/8/11 17:05
+* @version 1.0
+*/
+
+/**
+ * 任何⼀个⽀付模式；刷脸、指纹、密码，都会过不同程度的安全⻛控，这⾥定义⼀个安全校验接⼝
+ *
+ */
+public interface IPayMode {
+    boolean security(String uId);
+}
