eae449418dc83bc700a2648fffb6c46d30de0bba
马佳健 <majiajian@dongfangfuli.com>
2022-08-17 18:38:17 +0800

    享元模式

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/flyWeightMode/util/RedisUtils.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/flyWeightMode/util/RedisUtils.java
@@ -27,10 +27,12 @@ public class RedisUtils {
     private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
     private AtomicInteger stock = new AtomicInteger(0);
 
-    public RedisUtils() {scheduledExecutorService.scheduleAtFixedRate(() -> {
+    public RedisUtils() {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
             // 模拟库存消耗
-            stock.addAndGet(1);
+            int i = stock.addAndGet(1);
         }, 0, 100000, TimeUnit.MICROSECONDS);
+
     }
 
     public int getStockUsed() {
