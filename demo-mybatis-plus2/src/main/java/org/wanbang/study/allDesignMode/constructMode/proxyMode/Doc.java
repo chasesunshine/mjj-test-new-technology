6776a07cdacfc3b56f3219e343414a03aa1f58ef
马佳健 <majiajian@dongfangfuli.com>
2022-08-18 09:58:24 +0800

    代理模式

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/proxyMode/Doc.java
@@ -0,0 +1,10 @@
+package org.wanbang.study.allDesignMode.constructMode.proxyMode;
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/17 18:38
+* @version 1.0
+*/
+
+public class Doc {
+}
---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/constructMode/proxyMode/Test.java
@@ -0,0 +1,10 @@
+package org.wanbang.study.allDesignMode.constructMode.proxyMode;
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/17 18:39
+* @version 1.0
+*/
+
+public class Test {
+}
