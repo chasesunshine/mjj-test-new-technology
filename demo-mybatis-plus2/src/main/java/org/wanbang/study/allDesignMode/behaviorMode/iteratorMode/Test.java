e9c4571acb71929028f8dab0665e35d59224c07e
马佳健 <majiajian@dongfangfuli.com>
2022-08-19 13:59:01 +0800

    迭代器模式

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/behaviorMode/iteratorMode/Test.java
@@ -0,0 +1,50 @@
+package org.wanbang.study.allDesignMode.behaviorMode.iteratorMode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wanbang.study.allDesignMode.behaviorMode.iteratorMode.group.Employee;
+import org.wanbang.study.allDesignMode.behaviorMode.iteratorMode.group.GroupStructure;
+import org.wanbang.study.allDesignMode.behaviorMode.iteratorMode.group.Link;
+import org.wanbang.study.allDesignMode.behaviorMode.iteratorMode.lang.Iterator;
+import org.wanbang.study.allDesignMode.constructMode.appearanceMode.DoJoinPoint;
+
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/19 13:39
+* @version 1.0
+*/
+
+public class Test {
+    private static Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        // 数据填充
+        GroupStructure groupStructure = new GroupStructure("1", "⼩傅哥");
+
+        // 雇员信息
+        groupStructure.add(new Employee("2", "花花", "⼆级部⻔"));
+        groupStructure.add(new Employee("3", "⾖包", "⼆级部⻔"));
+        groupStructure.add(new Employee("4", "蹦蹦", "三级部⻔"));
+        groupStructure.add(new Employee("5", "⼤烧", "三级部⻔"));
+        groupStructure.add(new Employee("6", "⻁哥", "四级部⻔"));
+        groupStructure.add(new Employee("7", "玲姐", "四级部⻔"));
+        groupStructure.add(new Employee("8", "秋雅", "四级部⻔"));
+
+        // 节点关系 1->(1,2) 2->(4,5)
+        groupStructure.addLink("1", new Link("1", "2"));
+        groupStructure.addLink("1", new Link("1", "3"));
+        groupStructure.addLink("2", new Link("2", "4"));
+        groupStructure.addLink("2", new Link("2", "5"));
+        groupStructure.addLink("5", new Link("5", "6"));
+        groupStructure.addLink("5", new Link("5", "7"));
+        groupStructure.addLink("5", new Link("5", "8"));
+
+        Iterator<Employee> iterator = groupStructure.iterator();
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+            logger.info("{}，雇员 Id：{} Name：{}", employee.getDesc(), employee.getUId(), employee.getName());
+        }
+
+    }
+}
---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/behaviorMode/iteratorMode/lang/Iterator.java
@@ -0,0 +1,13 @@
+package org.wanbang.study.allDesignMode.behaviorMode.iteratorMode.lang;
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/19 13:50
+* @version 1.0
+*/
+
+public interface Iterator<E> {
+    boolean hasNext();
+    E next();
+
+}
