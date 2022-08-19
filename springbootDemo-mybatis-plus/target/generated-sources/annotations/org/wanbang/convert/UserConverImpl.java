364bd994f04448b918c94eb038b3507ad43c8f64
马佳健 <majiajian@dongfangfuli.com>
2022-08-19 14:52:19 +0800

    中介者模式

--- springbootDemo-mybatis-plus/target/generated-sources/annotations/org/wanbang/convert/UserConverImpl.java
+++ springbootDemo-mybatis-plus/target/generated-sources/annotations/org/wanbang/convert/UserConverImpl.java
@@ -8,7 +8,7 @@ import org.wanbang.entity.common.UserVo1;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-11T20:55:41+0800",
+    date = "2022-08-19T14:47:14+0800",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_221 (Oracle Corporation)"
 )
 @Component
