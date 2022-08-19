87445daefef040c8d0246c16ac2cd44a58d69c3d
马佳健 <majiajian@dongfangfuli.com>
2022-08-19 13:37:57 +0800

    命令模式

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/behaviorMode/commandMode/cuisine/service/impl/JiangSuCuisine.java
@@ -0,0 +1,24 @@
+package org.wanbang.study.allDesignMode.behaviorMode.commandMode.cuisine.service.impl;
+
+import org.wanbang.study.allDesignMode.behaviorMode.commandMode.cook.service.ICook;
+import org.wanbang.study.allDesignMode.behaviorMode.commandMode.cuisine.service.ICuisine;
+
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/19 13:22
+* @version 1.0
+*/
+
+public class JiangSuCuisine implements ICuisine {
+    private ICook cook;
+
+    public JiangSuCuisine(ICook cook) {
+        this.cook = cook;
+    }
+
+    public void cook() {
+        cook.doCooking();
+    }
+
+}
