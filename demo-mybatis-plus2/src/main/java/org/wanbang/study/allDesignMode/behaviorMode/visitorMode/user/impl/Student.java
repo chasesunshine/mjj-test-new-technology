cc89873fbdbe18bf3d48116159bf7380637d1dd3
马佳健 <majiajian@dongfangfuli.com>
2022-08-25 13:41:46 +0800

    访问者模式

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/allDesignMode/behaviorMode/visitorMode/user/impl/Student.java
@@ -0,0 +1,28 @@
+package org.wanbang.study.allDesignMode.behaviorMode.visitorMode.user.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.wanbang.study.allDesignMode.behaviorMode.visitorMode.user.User;
+import org.wanbang.study.allDesignMode.behaviorMode.visitorMode.visitor.Visitor;
+
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/25 13:32
+* @version 1.0
+*/
+public class Student extends User {
+
+    public Student(String name, String identity, String clazz) {
+        super(name, identity, clazz);
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public int ranking() {
+        return (int) (Math.random() * 100);
+    }
+
+}
