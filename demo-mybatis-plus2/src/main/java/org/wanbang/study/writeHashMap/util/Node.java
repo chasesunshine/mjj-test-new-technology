fe39612313809ddd5d4ba93e0a1e47ac46706c7d
马佳健 <majiajian@dongfangfuli.com>
2022-08-31 17:47:15 +0800

    HashMap底层原理 - 数组+链表

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/writeHashMap/util/Node.java
@@ -0,0 +1,38 @@
+package org.wanbang.study.writeHashMap.util;
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/8/31 17:17
+* @version 1.0
+*/
+
+/**
+ * 静态内部类
+ */
+public class Node<V> {
+    int hash;//hash值
+    Object key;//key
+    Object value;//value
+    Node<V> next;//指向下个节点（单例表）
+
+    Node(int hash, Object key, Object value, Node<V> next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public Object getKey() {
+        return this.key;
+    }
+
+    public Object getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + key + ", " + value + "}";
+    }
+
+}
