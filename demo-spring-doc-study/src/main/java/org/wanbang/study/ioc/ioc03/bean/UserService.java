a0f5a882a809836fac75bce2db659d1296412f25
马佳健 <majiajian@dongfangfuli.com>
2022-09-13 20:16:59 +0800

    第 04 章：对象实例化策略

--- demo-spring-doc-study/src/main/java/org/wanbang/study/ioc/ioc03/bean/UserService.java
+++ demo-spring-doc-study/src/main/java/org/wanbang/study/ioc/ioc03/bean/UserService.java
@@ -7,6 +7,11 @@ package org.wanbang.study.ioc.ioc03.bean;
 */
 
 public class UserService {
+//    private String name;
+//
+//    public UserService(String name) {
+//        this.name = name;
+//    }
 
     public void queryUserInfo(){
         System.out.println("查询用户信息");
