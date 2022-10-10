381f2495f466f155da67d16417ab9982046c9cfe
马佳健 <majiajian@dongfangfuli.com>
2022-10-10 20:10:21 +0800

    第 08 章：初始化和销毁方法

---
+++ demo-spring-doc-study/src/main/java/org/wanbang/study/ioc/ioc08/factory/InitializingBean.java
@@ -0,0 +1,23 @@
+package org.wanbang.study.ioc.ioc08.factory;
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/10/10 19:24
+* @version 1.0
+*/
+
+/**
+ *
+ *  InitializingBean、DisposableBean，两个接口方法还是比较常用的，在一些需要结
+ * 合 Spring 实现的组件中，经常会使用这两个方法来做一些参数的初始化和销毁操
+ * 作。比如接口暴漏、数据库数据读取、配置文件加载等等。
+ *
+ */
+public interface InitializingBean {
+    /**
+     * Bean 处理了属性填充后调用
+     *
+     * @throws Exception
+     */
+    void afterPropertiesSet() throws Exception;
+}
---
+++ demo-spring-doc-study/src/main/java/org/wanbang/study/ioc/ioc08/factory/support/DisposableBeanAdapter.java
@@ -0,0 +1,10 @@
+package org.wanbang.study.ioc.ioc08.factory.support;
+/**
+* @description: TODO
+* @author majiajian
+* @date 2022/10/10 19:23
+* @version 1.0
+*/
+
+public class DisposableBeanAdapter {
+}
