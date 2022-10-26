500b6520cf6d6ed175a5f0711d4768eafa20c18d
马佳健 <majiajian@dongfangfuli.com>
2022-10-26 14:49:08 +0800

    Code_27.java

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_26.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_26.java
@@ -1,10 +1,56 @@
 package org.wanbang.study.leetcode.codeAbality.easy;
 /**
-* @description: TODO
-* @author majiajian
-* @date 2022/10/26 14:09
-* @version 1.0
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，
+ * 返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+ * 由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，
+ * 如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
+ * 将最终结果插入 nums 的前 k 个位置后返回 k 。
+ * 不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1：
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2,_]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ *
+ * 示例 2：
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ *
+ * 提示：
+ * 1 <= nums.length <= 3 * 104
+ * -104 <= nums[i] <= 104
+ * nums 已按 升序 排列
+ *
+ *
 */
 
 public class Code_26 {
+    /**
+     * 思路：
+     * 使用双指针
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        int i = removeDuplicates(nums);
+        System.out.println(i);
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        // 使用双指针
+        if(nums==null || nums.length == 1){
+            return nums.length;
+        }
+        int i = 0,j =1;
+        while(j<nums.length){
+            if(nums[i]==nums[j]){
+                j++;
+            }else{
+                i++;
+                nums[i]=nums[j];
+                j++;
+            }
+        }
+        return i+1;
+    }
 }
