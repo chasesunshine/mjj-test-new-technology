1fae6b5985019f3e4ac809a63d4413880d8609d8
马佳健 <majiajian@dongfangfuli.com>
2022-10-26 10:26:13 +0800

    Code_14.java

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_14.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_14.java
@@ -41,7 +41,8 @@ public class Code_14 {
         String s=strs[0];
         for (String string : strs) {
             while(!string.startsWith(s)){
-                if(s.length()==0)return "";
+                if(s.length() == 0) return "";
+
                 //公共前缀不匹配就让它变短！
                 s=s.substring(0,s.length()-1);
             }
