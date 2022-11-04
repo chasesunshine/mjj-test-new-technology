1a8d4ef8ffbbad3d216648a0e0c230a9efc485c4
马佳健 <majiajian@dongfangfuli.com>
2022-11-04 17:00:31 +0800

    Code_108.java

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_108.java
@@ -0,0 +1,82 @@
+package org.wanbang.study.leetcode.codeAbality.easy;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ *
+ * 示例 1：
+ * 输入：nums = [-10,-3,0,5,9]
+ * 输出：[0,-3,9,-10,null,5]
+ * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+ *
+ * 示例 2：
+ * 输入：nums = [1,3]
+ * 输出：[3,1]
+ * 解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+ *
+ * 提示：
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums 按 严格递增 顺序排列
+ */
+
+public class Code_108 {
+    /**
+     * 思路：
+     * 递归
+     */
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode().setVal(0)
+                .setLeft(new TreeNode().setVal(-3)
+                        .setLeft(new TreeNode().setVal(-10)
+                                .setRight(null))
+                )
+                .setRight(new TreeNode().setVal(9)
+                        .setLeft(new TreeNode().setVal(5))
+                        .setRight(null)
+                );
+
+        System.out.println(treeNode.toString());
+    }
+
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        // 左右等分建立左右子树，中间节点作为子树根节点，递归该过程
+        return nums == null ? null : buildTree(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode buildTree(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int m = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = buildTree(nums, l, m - 1);
+        root.right = buildTree(nums, m + 1, r);
+        return root;
+    }
+
+    @Data
+    @Accessors(chain = true)
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
