f71f47fafd2c9fd5259ebe21370b67114e7cd124
马佳健 <majiajian@dongfangfuli.com>
2022-11-17 23:23:43 +0800

    mybatisPlus代码自动生成

---
+++ demo-mybatis-plus3/src/main/java/org/wanbang/controller/UserController.java
@@ -0,0 +1,20 @@
+package org.wanbang.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author jjh
+ * @since 2022-11-17
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
---
+++ demo-mybatis-plus3/src/main/java/org/wanbang/dao/UserDao.java
@@ -0,0 +1,16 @@
+package org.wanbang.dao;
+
+import org.wanbang.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author jjh
+ * @since 2022-11-17
+ */
+public interface UserDao extends BaseMapper<User> {
+
+}
