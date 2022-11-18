415f8850dac1fd398cafffa0628a0c89be83acf8
马佳健 <majiajian@dongfangfuli.com>
2022-11-18 11:16:30 +0800

    mybatisPlus代码自动生成

---
+++ src/main/java/org/wanbang/service/UserService.java
@@ -0,0 +1,16 @@
+package org.wanbang.service;
+
+import org.wanbang.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author jjh
+ * @since 2022-11-18
+ */
+public interface UserService extends IService<User> {
+
+}
