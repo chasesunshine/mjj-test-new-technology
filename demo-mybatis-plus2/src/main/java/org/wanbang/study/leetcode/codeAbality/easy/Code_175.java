c84882591c27a2bcd051fb28ff6bc3ef4ce2b562
马佳健 <majiajian@dongfangfuli.com>
2022-12-16 14:49:13 +0800

    Code_175.java

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_175.java
@@ -0,0 +1,19 @@
+package org.wanbang.study.leetcode.codeAbality.easy;
+/**
+* @description: sql
+* @author majiajian
+* @date 2022/12/16 14:41
+* @version 1.0
+*/
+
+public class Code_175 {
+    /**
+     * 思路：sql语句
+     *
+     * # Write your MySQL query statement below
+     * select p.firstName,p.lastName ,a.city,a.state from Person p left join Address a on a.personId  = p.personId
+     */
+    public static void main(String[] args) {
+    }
+
+}
