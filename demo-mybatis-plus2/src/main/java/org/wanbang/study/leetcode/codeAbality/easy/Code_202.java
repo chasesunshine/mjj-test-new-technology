f4b4360c025c0bf65979ffdcbbbcde5d25725d9a
马佳健 <majiajian@dongfangfuli.com>
2022-12-17 14:44:50 +0800

    Code_202.java - 利用hashset

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_202.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_202.java
@@ -1,4 +1,8 @@
 package org.wanbang.study.leetcode.codeAbality.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 编写一个算法来判断一个数 n 是不是快乐数。
  * 「快乐数」 定义为：
@@ -22,6 +26,24 @@ package org.wanbang.study.leetcode.codeAbality.easy;
  */
 
 public class Code_202 {
+    /**
+     * 思路：
+     *  根据我们的探索，我们猜测会有以下三种可能。
+     * 最终会得到 111。
+     * 最终会进入循环。
+     * 值会越来越大，最后接近无穷大。
+     *
+     *算法
+     * 算法分为两部分，我们需要设计和编写代码。
+     * 给一个数字 nnn，它的下一个数字是什么？
+     * 按照一系列的数字来判断我们是否进入了一个循环。
+     * 第 1 部分我们按照题目的要求做数位分离，求平方和。
+     * 第 2 部分可以使用哈希集合完成。每次生成链中的下一个数字时，我们都会检查它是否已经在哈希集合中。
+     * 如果它不在哈希集合中，我们应该添加它。
+     * 如果它在哈希集合中，这意味着我们处于一个循环中，因此应该返回 false。
+     * 我们使用哈希集合而不是向量、列表或数组的原因是因为我们反复检查其中是否存在某数字。检查数字是否在哈希集合中需要 O(1)O(1)O(1) 的时间，而对于其他数据结构，则需要 O(n)O(n)O(n) 的时间。选择正确的数据结构是解决这些问题的关键部分。
+     *
+     */
     public static void main(String[] args) {
         int n = 19;
         boolean happy = isHappy(n);
@@ -29,23 +51,23 @@ public class Code_202 {
     }
 
     public static boolean isHappy(int n) {
-        if(!(n == 1 || n == 7)){
-            return false;
-        }else {
-            int sum = 0;
-            while (n > 0) {
-                // 对10取模得最后一位数字
-                int remainder = n % 10;
-                // 移除最后一位数字
-                n = n / 10;
-                sum = sum + remainder * remainder;
-            }
-            if (sum != 1) {
-                isHappy(sum);
-            }
-
-            return true;
+        Set<Integer> circle = new HashSet<>();
+        while (n != 1 && !circle.contains(n)){
+            circle.add(n);
+            n = getSum(n);
         }
+        return n == 1;
     }
+
+    private static int getSum(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
 
 }
