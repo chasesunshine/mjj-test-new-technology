b887056de71cc176d5894fc10b630bf486945dc5
马佳健 <majiajian@dongfangfuli.com>
2022-12-27 16:51:40 +0800

    Code_205.java - 双向映射，这个思路很棒

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_205.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_205.java
@@ -1,4 +1,7 @@
 package org.wanbang.study.leetcode.codeAbality.easy;
+
+import java.util.HashMap;
+
 /**
  * 给定两个字符串 s 和 t ，判断它们是否是同构的。
  * 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
@@ -19,16 +22,39 @@ package org.wanbang.study.leetcode.codeAbality.easy;
 
 public class Code_205 {
 
+    /**
+     * 思路：建立双向映射
+     * 前提：两个字符串长度需一致
+     */
     public static void main(String[] args) {
-        String a ="";
-        String b ="";
+        String a ="add";
+        String b ="egg";
 
         boolean isomorphic = isIsomorphic(a, b);
         System.out.println(isomorphic);
     }
 
     public static boolean isIsomorphic(String s, String t) {
-
-        return false;
+         /*
+        HashMap建立双向映射
+         */
+        int len = s.length();
+        HashMap<Character, Character> map1 = new HashMap<>();   // s[i]->t[i]
+        HashMap<Character, Character> map2 = new HashMap<>();   // t[i]->s[i]
+        for (int i = 0; i < len; i++) {
+            char ch1 = s.charAt(i), ch2 = t.charAt(i);
+            if (!map1.containsKey(ch1)) {
+                map1.put(ch1, ch2);
+            }
+            if (!map2.containsKey(ch2)) {
+                map2.put(ch2, ch1);
+            }
+            // 均存在的情况:排除掉不匹配的情况
+            if (map1.get(ch1) != ch2 || map2.get(ch2) != ch1){
+                return false;
+            }
+        }
+        // 最后所有都匹配的情况
+        return true;
     }
 }
