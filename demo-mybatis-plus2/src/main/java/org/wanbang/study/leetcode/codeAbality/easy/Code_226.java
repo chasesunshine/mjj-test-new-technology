cf5814b500af1666807bc5f9eb74f38ff5a8a71d
马佳健 <majiajian@dongfangfuli.com>
2022-12-28 20:41:14 +0800

    Code_226.java - 递归思想

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_226.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_226.java
@@ -1,4 +1,10 @@
 package org.wanbang.study.leetcode.codeAbality.easy;
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.wanbang.study.leetcode.codeAbality.Code110;
+
 /**
  * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
  *
@@ -13,8 +19,68 @@ package org.wanbang.study.leetcode.codeAbality.easy;
  * 示例 3：
  * 输入：root = []
  * 输出：[]
-*/
+ */
 
 public class Code_226 {
+    /**
+     * 思路：
+     * 可以发现想要翻转它，其实就把每一个节点的左右孩子交换一下就可以了。
+     * 关键在于遍历顺序，前中后序应该选哪一种遍历顺序？ （一些同学这道题都过了，但是不知道自己用的是什么顺序）
+     * 遍历的过程中去翻转每一个节点的左右孩子就可以达到整体翻转的效果。
+     *
+     * 注意只要把每一个节点的左右孩子翻转一下，就可以达到整体翻转的效果
+     * 这道题目使用前序遍历和后序遍历都可以，唯独中序遍历不方便，因为中序遍历会把某些节点的左右孩子翻转了两次！建议拿纸画一画，就理解了
+     * 那么层序遍历可以不可以呢？依然可以的！只要把每一个节点的左右孩子翻转一下的遍历方式都是可以的！
+     */
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode().setVal(4)
+                .setLeft(new TreeNode().setVal(2)
+                        .setLeft(new TreeNode().setVal(1))
+                        .setRight(new TreeNode().setVal(3))
+                )
+                .setRight(new TreeNode().setVal(7)
+                        .setLeft(new TreeNode().setVal(6))
+                        .setRight(new TreeNode().setVal(9))
+                );
+
+        TreeNode treeNode1 = invertTree(treeNode);
+
+        System.out.println(JSON.toJSONString(treeNode1));
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        swapChildren(root);
+        return root;
+    }
+
+    private static void swapChildren(TreeNode root) {
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+
+    @Data
+    @Accessors(chain = true)
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {
+
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
 }
