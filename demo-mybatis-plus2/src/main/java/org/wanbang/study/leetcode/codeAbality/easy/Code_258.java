7c9fc0322acabbaaac5c87cbeac41ed20873b372
马佳健 <majiajian@dongfangfuli.com>
2022-12-30 17:39:40 +0800

    Code_257.java - 递归的思路

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/leetcode/codeAbality/easy/Code_258.java
@@ -0,0 +1,22 @@
+package org.wanbang.study.leetcode.codeAbality.easy;
+/**
+ * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
+ *
+ * 示例 1:
+ * 输入: num = 38
+ * 输出: 2
+ * 解释: 各位相加的过程为：
+ * 38 --> 3 + 8 --> 11
+ * 11 --> 1 + 1 --> 2
+ * 由于 2 是一位数，所以返回 2。
+ *
+ * 示例 2:
+ * 输入: num = 0
+ * 输出: 0
+*/
+
+public class Code_258 {
+    public static void main(String[] args) {
+
+    }
+}
