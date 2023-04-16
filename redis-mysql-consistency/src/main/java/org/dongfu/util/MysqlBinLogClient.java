8223a8f1956fcd4eabee29c3d6e93653642181c1
马佳健 <majiajian@dongfangfuli.com>
2023-04-16 15:38:38 +0800

    redis-mysql双写一致性

--- redis-mysql-consistency/src/main/java/org/dongfu/util/MysqlBinLogClient.java
+++ redis-mysql-consistency/src/main/java/org/dongfu/util/MysqlBinLogClient.java
@@ -15,6 +15,10 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.BitSet;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 //此类可以监控MySQL库数据的增删改
 @Component
@@ -22,6 +26,7 @@ import java.io.IOException;
 //在SpringBoot中，提供了一个接口：ApplicationRunner。
 //该接口中，只有一个run方法，他执行的时机是：spring容器启动完成之后，就会紧接着执行这个接口实现类的run方法。
 public class MysqlBinLogClient implements ApplicationRunner {
+    private static ConcurrentHashMap<Long,String> tableIdMap = new ConcurrentHashMap<>();
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -40,46 +45,64 @@ public class MysqlBinLogClient implements ApplicationRunner {
 
         //自己MySQL的信息。host，port，username，password
         BinaryLogClient client = new BinaryLogClient("localhost", 3306, "root", "root");
-        /**因为binlog不是以数据库为单位划分的，所以监控binglog不是监控的单个的数据库，而是整个当前所设置连接的MySQL，
+        /**
+         * 因为binlog不是以数据库为单位划分的，所以监控binglog不是监控的单个的数据库，而是整个当前所设置连接的MySQL，
          *其中任何一个库发生数据增删改，这里都能检测到，
          *所以不用设置所监控的数据库的名字(我也不知道怎么设置，没发现有包含这个形参的构造函数)
          *如果需要只监控指定的数据库，可以看后面代码，可以获取到当前发生变更的数据库名称。可以根据名称来决定是否监控
+         *
          **/
 
         client.setServerId(100); //和自己之前设置的server-id保持一致，但是我不知道为什么不一致也能成功
 
         //下面直接照抄就行
-        client.registerEventListener(event -> {
-            EventData data = event.getData();
-            if (data instanceof TableMapEventData) {
-                //只要连接的MySQL发生的增删改的操作，则都会进入这里，无论哪个数据库
+        client.registerEventListener(
+                event -> {
+                    EventData data = event.getData();
+                    if (data instanceof TableMapEventData) {
+                        //只要连接的MySQL发生的增删改的操作，则都会进入这里，无论哪个数据库
 
-                TableMapEventData tableMapEventData = (TableMapEventData) data;
+                        TableMapEventData tableMapEventData = (TableMapEventData) data;
 
-                //可以通过转成TableMapEventData类实例的tableMapEventData来获取当前发生变更的数据库
-                System.out.println("发生变更的数据库："+tableMapEventData.getDatabase());
+                        //可以通过转成TableMapEventData类实例的tableMapEventData来获取当前发生变更的数据库
+                        System.out.println("发生变更的数据库："+tableMapEventData.getDatabase());
 
-                System.out.print("TableID:");
-                //表ID
-                System.out.println(tableMapEventData.getTableId());
-                System.out.print("TableName:");
-                //表名字
-                System.out.println(tableMapEventData.getTable());
-            }
-            //表数据发生修改时触发
-            if (data instanceof UpdateRowsEventData) {
-                System.out.println("Update:");
-                System.out.println(data.toString());
-                //表数据发生插入时触发
-            } else if (data instanceof WriteRowsEventData) {
-                System.out.println("Insert:");
-                System.out.println(data.toString());
-                //表数据发生删除后触发
-            } else if (data instanceof DeleteRowsEventData) {
-                System.out.println("Delete:");
-                System.out.println(data.toString());
-            }
-        });
+                        System.out.print("TableID:");
+                        //表ID
+                        System.out.println(tableMapEventData.getTableId());
+                        System.out.print("TableName:");
+                        //表名字
+                        System.out.println(tableMapEventData.getTable());
+
+                        tableIdMap.put(tableMapEventData.getTableId(),tableMapEventData.getTable());
+                    }
+
+                    //表数据发生修改时触发
+                    if (data instanceof UpdateRowsEventData) {
+                        String table = tableIdMap.get(((UpdateRowsEventData) data).getTableId());
+                        if(table.equals("redis_mysql_consistency")){
+                            System.out.println("Update:");
+                            System.out.println(data.toString());
+                        }
+
+                        //表数据发生插入时触发
+                    } else if (data instanceof WriteRowsEventData) {
+                        String table = tableIdMap.get(((WriteRowsEventData) data).getTableId());
+                        if(table.equals("redis_mysql_consistency")){
+                            System.out.println("Insert:");
+                            System.out.println(data.toString());
+                        }
+
+                        //表数据发生删除后触发
+                    } else if (data instanceof DeleteRowsEventData) {
+                        String table = tableIdMap.get(((DeleteRowsEventData) data).getTableId());
+                        if(table.equals("redis_mysql_consistency")){
+                            System.out.println("Delete:");
+                            System.out.println(data.toString());
+                        }
+
+                    }
+                });
 
         try {
             client.connect();
