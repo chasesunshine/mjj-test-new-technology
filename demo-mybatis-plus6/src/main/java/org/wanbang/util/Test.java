9a67eb56e8ff2d635c7267e222abb23e0086b3f3
马佳健 <majiajian@dongfangfuli.com>
2023-05-11 14:43:16 +0800

    如果⽗类中只定义了有参数的构造⽅法，⽽在⼦类的构造⽅法中 ⼜没有⽤ super() 来调⽤⽗类中特定的构造⽅法，则编译时将发⽣错误，因为 Java 程序在⽗类中找 不到没有参数的构造⽅法可供执⾏。解决办法是在⽗类⾥加上⼀个不做事且没有参数的构造⽅法。

---
+++ demo-mybatis-plus6/src/main/java/org/wanbang/util/Test.java
@@ -0,0 +1,7 @@
+package org.wanbang.util;
+
+public class Test {
+    public Test(String a ){
+
+    }
+}
---
+++ demo-mybatis-plus6/src/main/java/org/wanbang/util/TestSon.java
@@ -0,0 +1,8 @@
+package org.wanbang.util;
+
+public class TestSon extends Test{
+
+    public TestSon(String a) {
+        super(a);
+    }
+}
