956a91f6217e9ce3a720dfa8bcf1fb4a73417a2d
chasesunshine <1061902084@qq.com>
2023-07-18 15:35:11 +0800

    synchronized用法详解

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/synchronizedConditonTest/actionObjectIsClass/SynchronizedUsage.java
@@ -0,0 +1,33 @@
+package org.wanbang.study.synchronizedConditonTest.actionObjectIsClass;
+
+/**
+ * @author yedashi
+ * @version 1.0
+ * @date 2022/5/16 11:41
+ * @description
+ */
+public class SynchronizedUsage {
+
+    private void print() {
+        String name = Thread.currentThread().getName();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello " + name);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 同步代码块——锁的对象是类
+     */
+    public void synchronizedCodelockClass(){
+        // 这里锁住的是类
+        synchronized (SynchronizedUsage.class){
+            print();
+        }
+    }
+
+}
---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/synchronizedConditonTest/actionObjectIsGeneralMethod/Test1.java
@@ -0,0 +1,23 @@
+package org.wanbang.study.synchronizedConditonTest.actionObjectIsGeneralMethod;
+
+public class Test1{
+
+    public synchronized void test() {
+
+    }
+
+}
+
+//等价于
+/*
+public class Test1{
+
+    public void test() {
+        //锁的是当前对象
+        synchronized(this) {
+
+        }
+    }
+
+}
+*/
