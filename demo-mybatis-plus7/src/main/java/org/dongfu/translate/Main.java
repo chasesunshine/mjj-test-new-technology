195db393a6a7e40ee79c489e1b37653a2e61cccd
马佳健 <majiajian@dongfangfuli.com>
2023-08-08 01:14:55 +0800

    百度翻译开放平台： http://api.fanyi.baidu.com/doc/21

---
+++ demo-mybatis-plus7/src/main/java/org/dongfu/translate/Main.java
@@ -0,0 +1,38 @@
+package org.dongfu.translate;
+
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.dongfu.translate.util.TransApi;
+
+public class Main {
+
+    // 在平台申请的APP_ID 详见 http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+    private static final String APP_ID = "20230807001772832";
+    private static final String SECURITY_KEY = "ouTBvSgyIelixsDS7nYx";
+    private static TransApi api = new TransApi(APP_ID, SECURITY_KEY);
+
+    public static void main(String[] args) {
+
+        String query = "苹果";
+        String transResult = api.getTransResult(query, "zh", "en");
+        JSONObject jsonObject = JSONObject.parseObject(transResult);
+        JSONArray trans_result = jsonObject.getJSONArray("trans_result");
+        Object dst = trans_result.get(0);
+        JSONObject jsonObjectDst = JSONObject.parseObject(dst.toString());
+        Object dstFinal = jsonObjectDst.get("dst");
+        System.out.println(dstFinal.toString());
+    }
+
+    public String translate(char c,String srcLanguage,String destLanguage){
+        String s = String.valueOf(c);
+        String transResult = api.getTransResult(s, srcLanguage, destLanguage);
+        JSONObject jsonObject = JSONObject.parseObject(transResult);
+        JSONArray trans_result = jsonObject.getJSONArray("trans_result");
+        Object dst = trans_result.get(0);
+        JSONObject jsonObjectDst = JSONObject.parseObject(dst.toString());
+        Object dstFinal = jsonObjectDst.get("dst");
+        return dstFinal.toString();
+    }
+
+}
