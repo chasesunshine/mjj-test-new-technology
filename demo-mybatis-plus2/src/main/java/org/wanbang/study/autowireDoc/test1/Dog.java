ff31ec681a3d5bb17fb53992ee94d2256b1d0282
chasesunshine <1061902084@qq.com>
2023-10-27 12:03:16 +0800

    spring——自动装配【非常详细】

--- demo-mybatis-plus2/src/main/java/org/wanbang/study/autowireDoc/test1/Dog.java
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/autowireDoc/test1/Dog.java
@@ -1,9 +1,18 @@
 package org.wanbang.study.autowireDoc.test1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
 public class Dog {
     public Dog(){
         System.out.println("执行 dog构造方法");
     }
+
+//    @Bean
+//    public Dog getTestDog(){
+//        return new Dog();
+//    }
 
     public void shout(){
         System.out.println("wang wang~");
