8ea662d222748e0ecc026d2bfddf96ffbbab8e61
chasesunshine <1061902084@qq.com>
2023-10-27 17:22:03 +0800

    spring——自动装配【非常详细】

---
+++ demo-mybatis-plus2/src/main/java/org/wanbang/study/autowireDoc/importantDoc/Doc.java
@@ -0,0 +1,8 @@
+package org.wanbang.study.autowireDoc.importantDoc;
+
+/**
+ * https://blog.csdn.net/qq_57434877/article/details/123933529?spm=1001.2101.3001.6650.4&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4-123933529-blog-129818815.235%5Ev38%5Epc_relevant_anti_t3&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-4-123933529-blog-129818815.235%5Ev38%5Epc_relevant_anti_t3&utm_relevant_index=9
+ *
+ */
+public class Doc {
+}
