c3ae85d4f357806d25afc000817d21b4a69e4bf7
chasesunshine <1061902084@qq.com>
2024-04-16 12:08:01 +0800

    TDEngine 个人demo 测试

---
+++ demo-mybatis-plus-edengine/src/main/java/org/wanbang/JNIConnectRestBatchExample.java
@@ -0,0 +1,23 @@
+package org.wanbang;
+
+import com.taosdata.jdbc.TSDBDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JNIConnectRestBatchExample {
+    public static void main(String[] args) throws SQLException {
+        String jdbcUrl = "jdbc:TAOS-RS://HaoD:6041?user=root&password=taosdata";
+        Properties connProps = new Properties();
+        connProps.setProperty(TSDBDriver.PROPERTY_KEY_BATCH_LOAD, "true");
+        Connection conn = DriverManager.getConnection(jdbcUrl, connProps);
+        System.out.println("Connected");
+        conn.close();
+    }
+}
+
+// use
+// String jdbcUrl = "jdbc:TAOS://localhost:6030/dbName?user=root&password=taosdata";
+// if you want to connect a specified database named "dbName".
---
+++ demo-mybatis-plus-edengine/src/main/java/org/wanbang/JNIConnectRestExample.java
@@ -0,0 +1,21 @@
+package org.wanbang;
+
+import com.taosdata.jdbc.TSDBDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JNIConnectRestExample {
+    public static void main(String[] args) throws SQLException {
+        String jdbcUrl = "jdbc:TAOS-RS://HaoD:6041?user=root&password=taosdata";
+        Connection conn = DriverManager.getConnection(jdbcUrl);
+        System.out.println("Connected");
+        conn.close();
+    }
+}
+
+// use
+// String jdbcUrl = "jdbc:TAOS://localhost:6030/dbName?user=root&password=taosdata";
+// if you want to connect a specified database named "dbName".
