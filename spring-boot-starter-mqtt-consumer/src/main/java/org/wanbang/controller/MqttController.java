db3d8a05d68598bc889198c53a3ab73ca82cb4d1
chasesunshine <1061902084@qq.com>
2024-07-05 17:23:57 +0800

    commit - spring-boot-starter-mqtt-provider spring-boot-starter-mqtt-consumer

---
+++ spring-boot-starter-mqtt-consumer/src/main/java/org/wanbang/controller/MqttController.java
@@ -0,0 +1,29 @@
+package org.wanbang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.wanbang.config.MqttConsumerConfig;
+
+@Controller
+public class MqttController {
+    @Autowired
+    private MqttConsumerConfig client;
+
+    @Value("${spring.mqtt.client.id}")
+    private String clientId;
+    @RequestMapping("/connect")
+    @ResponseBody
+    public String connect(){
+        client.connect();
+        return clientId + "连接到服务器";
+    }
+    @RequestMapping("/disConnect")
+    @ResponseBody
+    public String disConnect(){
+        client.disConnect();
+        return clientId + "与服务器断开连接";
+    }
+}
---
+++ spring-boot-starter-mqtt-provider/src/main/java/org/wanbang/config/MqttProviderCallBack.java
@@ -0,0 +1,42 @@
+package org.wanbang.config;
+
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+public class MqttProviderCallBack implements MqttCallback {
+
+//    @Value("${mqtt.client.id}")
+//    private String clientId;
+
+    /**
+     * 与服务器断开连接的回调
+     */
+    @Override
+    public void connectionLost(Throwable throwable) {
+//        System.out.println(clientId + "与服务器断开连接");
+    }
+
+    /**
+     * 消息到达的回调
+     */
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+
+    }
+
+    /**
+     * 消息发布成功的回调
+     */
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+        IMqttAsyncClient client = iMqttDeliveryToken.getClient();
+        System.out.println(client.getClientId() + "发布消息成功！");
+    }
+}
