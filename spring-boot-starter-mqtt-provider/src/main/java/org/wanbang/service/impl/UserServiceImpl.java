9bc99c0a0e942c37bbefaa3b3568bf2d53730cb7
chasesunshine <1061902084@qq.com>
2024-07-05 16:24:04 +0800

    commit - spring-boot-starter-mqtt-provider spring-boot-starter-mqtt-consumer

---
+++ spring-boot-starter-mqtt-provider/src/main/java/org/wanbang/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package org.wanbang.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.wanbang.dao.UserDao;
+import org.wanbang.entity.User;
+import org.wanbang.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * (SpringWord)表服务实现类
+ *
+ * @author makejava
+ * @since 2022-06-16 10:17:43
+ */
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserDao userDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public User queryById(Long id) {
+        log.info("通过ID查询单条数据");
+        return this.userDao.selectById(id);
+    }
+
+}
