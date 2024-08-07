29292a8a8d3046e15f6992fc410aad3cc2a88192
chasesunshine <1061902084@qq.com>
2024-08-07 11:04:42 +0800

    commit

--- demo-mybatis-plus10/src/main/java/org/wanbang/controller/RocketMQProducer.java
+++ demo-mybatis-plus10/src/main/java/org/wanbang/controller/RocketMQProducer.java
@@ -3,9 +3,12 @@ package org.wanbang.controller;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.rocketmq.acl.common.AclClientRPCHook;
 import org.apache.rocketmq.acl.common.SessionCredentials;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -17,6 +20,32 @@ import javax.crypto.spec.SecretKeySpec;
 public class RocketMQProducer {
 
     public static void main(String[] args) throws Exception {
+        extracted();
+//        extracted1();
+    }
+
+    private static void extracted1() throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
+        AclClientRPCHook auth = new AclClientRPCHook(new SessionCredentials("IotAdmin", "Daikin@iot"));
+        // 设置生产者组名
+        DefaultMQProducer producer = new DefaultMQProducer("iot_device_room_group",auth);
+        // 指定NameServer地址，多个地址用分号分隔
+        producer.setNamesrvAddr("124.71.159.144:8200");
+        // 启动生产者
+        producer.start();
+
+        try {
+            // 创建消息实例，指定主题、标签和消息体
+            Message message = new Message("topic_ctc_sync_room", "*", "-------Hello rain-----".getBytes());
+            // 发送消息
+            SendResult send = producer.send(message, 10000);
+            System.out.println("Message sent.");
+        } finally {
+            // 关闭生产者
+            producer.shutdown();
+        }
+    }
+
+    private static void extracted() throws Exception {
         AclClientRPCHook auth = new AclClientRPCHook(new SessionCredentials("IotAdmin", "Daikin@iot"));
         // 设置生产者组名
         DefaultMQProducer producer = new DefaultMQProducer("dev-EquipmentHourlyDataQueue-Group",auth);
