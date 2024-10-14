5fa0c7a0cbf4ff278be76c295fe901da2091d945
chasesunshine <1061902084@qq.com>
2024-10-14 15:42:13 +0800

    redis获取过期时间

--- demo-mybatis-plus9/src/test/java/org/wanbang/LruCacheManagerTest.java
+++ demo-mybatis-plus9/src/test/java/org/wanbang/LruCacheManagerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.wanbang.lru.LruCacheManager;
 import org.wanbang.redisapi.RedisUtil;
@@ -24,6 +26,10 @@ public class LruCacheManagerTest {
 
     @Resource
     private RedisUtil redisUtil;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
 
     @Test
     public void testLruCache() throws InterruptedException {
