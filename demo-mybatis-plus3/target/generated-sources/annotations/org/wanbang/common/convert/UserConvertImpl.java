4411d3166fda1223307afacd0c2a15c4317d6ddb
chasesunshine <1061902084@qq.com>
2024-10-21 18:17:15 +0800

    springcloud-gateway demo

--- demo-mybatis-plus3/target/generated-sources/annotations/org/wanbang/common/convert/UserConvertImpl.java
+++ demo-mybatis-plus3/target/generated-sources/annotations/org/wanbang/common/convert/UserConvertImpl.java
@@ -7,7 +7,7 @@ import org.wanbang.entity.SpringWorld;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-21T17:07:45+0800",
+    date = "2024-10-21T18:05:48+0800",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_282 (Azul Systems, Inc.)"
 )
 @Component
