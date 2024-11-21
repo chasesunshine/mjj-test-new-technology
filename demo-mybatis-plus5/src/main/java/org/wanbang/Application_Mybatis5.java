a29177ebb09dc68ed044e64feb474aa120f4b480
ma.jiajian <1061902084@qq.com>
2024-11-21 11:03:54 +0800

    @EnableAspectJAutoProxy在springboot一定要加吗

--- demo-mybatis-plus5/src/main/java/org/wanbang/Application_Mybatis5.java
+++ demo-mybatis-plus5/src/main/java/org/wanbang/Application_Mybatis5.java
@@ -3,7 +3,9 @@ package org.wanbang;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 @MapperScan("org.wanbang.dao")	//程序会自动为mybatis创建代理对象
 public class Application_Mybatis5 {
