188533979277e3c27aad5af529dc099fd30a17b7
ma.jiajian <1061902084@qq.com>
2025-01-20 16:43:47 +0800

    commit

--- demo-mybatis-plus17/src/main/java/org/wanbang/entity/Order.java
+++ demo-mybatis-plus17/src/main/java/org/wanbang/entity/Order.java
@@ -1,12 +1,10 @@
 package org.wanbang.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
