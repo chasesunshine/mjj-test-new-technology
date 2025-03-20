1369cfe5dcb3f9adf8925365b1434dcec187f0c2
ma.jiajian <1061902084@qq.com>
2025-03-20 15:52:36 +0800

    CountDownLatch 模拟并发调用同一个方法

--- demo-mybatis-plus14/src/main/java/org/wanbang/controller/UserController.java
+++ demo-mybatis-plus14/src/main/java/org/wanbang/controller/UserController.java
@@ -9,6 +9,7 @@ import org.wanbang.entity.User;
 import org.wanbang.service.UserService;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * (SpringWord)表控制层
@@ -29,11 +30,53 @@ public class UserController {
         return JSON.toJSONString(user);
     }
 
+    @GetMapping("/selectTwo")
+    public String selectTwo() throws InterruptedException {
+        int numThreads = 5;
+        // 用于控制线程同时开始
+        CountDownLatch startLatch = new CountDownLatch(1);
+        // 用于等待所有线程完成
+        CountDownLatch endLatch = new CountDownLatch(numThreads);
+
+        // 创建多个线程
+        for (int i = 0; i < numThreads; i++) {
+            int threadId = i;
+            new Thread(() -> {
+                try {
+                    startLatch.await(); // 等待主线程发出开始信号
+
+                    // 集控器上电
+//                    ForwardCtcMqttToMqMessage forwardCtcMqttToMqMessage = new ForwardCtcMqttToMqMessage();
+//                    mqttTopicOnlineHandler.handleMessage(forwardCtcMqttToMqMessage);
+                    testMethod();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    endLatch.countDown(); // 标记当前线程完成
+                }
+            }).start();
+        }
+
+        System.out.println("All threads are ready, starting concurrent execution...");
+        // 释放所有线程，开始并发执行
+        startLatch.countDown();
+
+        // 等待所有线程完成
+        endLatch.await();
+        System.out.println("All threads have finished execution.");
+        return null;
+    }
+
     @GetMapping("/insetOne")
     public Integer insetOne() {
         Integer count = userService.insertData();
         return count;
     }
+
+    private void testMethod() {
+        System.out.println("123");
+    }
+
 
 }
 
