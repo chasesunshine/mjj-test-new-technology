195c821ab3dddf23942d6b1d2735ae177a520712
ma.jiajian <1061902084@qq.com>
2025-04-10 15:17:26 +0800

    hadoop从安装到springboot集成 - hadoop测试结果.png

--- demo-mybatis-plus19/src/main/java/org/wanbang/controller/HadoopController.java
+++ demo-mybatis-plus19/src/main/java/org/wanbang/controller/HadoopController.java
@@ -26,28 +26,33 @@ public class HadoopController {
      */
     @Value("${hadoop.input.path}")
     private String inputPath;
+
     /**
      * 上传本地文件
-     * @param srcFile 文件路径
+     *
+     * @param uploadDataReq 上传的文件
      * @return
-     * @throws IOException
+     * @throws Exception
      */
     @PostMapping("/uploadLocal")
     public String uploadlocal(@RequestBody UploadDataReq uploadDataReq) throws Exception {
         //上传时需要补全文件名
         String uploadUrl = fsUri+inputPath+"xx.csv";
+        // "srcFile": "C://Users//daikin//Desktop//工作记录//GPF Cloud式样书清单整理.xlsx"
         hadoopUtil.uploadLocalFileToHDFS(uploadDataReq.getSrcFile(), uploadUrl);
         return "upload";
     }
 
     /**
      * 下载文件
-     * @param srcFile 文件名
+     *
+     * @param downloadDataReq hadoop上的文件名称
      * @param response
      */
     @PostMapping("/download")
     public void downloadFile(@RequestBody DownloadDataReq downloadDataReq, HttpServletResponse response) {
         //下载文件名及路径
+        // "srcFile": "xx.csv"
         String hdfsFilePath=fsUri+inputPath+"/"+downloadDataReq.getSrcFile();
         response.setContentType("application/octet-stream");
         response.setHeader("Content-Disposition", "attachment; filename=" + hdfsFilePath.substring(hdfsFilePath.lastIndexOf('/') + 1));
